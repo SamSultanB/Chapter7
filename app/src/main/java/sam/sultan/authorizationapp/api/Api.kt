d93e01305545c8d5353dcb5cd323137d3e8e9614
lux <ssbekmuratov@gmail.com>
2023-06-06 22:31:35 +0200

    data classes

--- app/src/main/java/sam/sultan/authorizationapp/api/Api.kt
+++ app/src/main/java/sam/sultan/authorizationapp/api/Api.kt
@@ -1,5 +1,19 @@
 package sam.sultan.authorizationapp.api
 
+import retrofit2.http.POST
+
 interface Api {
+
+    @POST("register")
+    suspend fun register()
+
+    @POST("login")
+    suspend fun login()
+
+    @POST("{user_id}/form")
+    suspend fun infoForm()
+
+    @POST("forgot_password")
+    suspend fun password()
 
 }
